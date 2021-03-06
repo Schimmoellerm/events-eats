62250211f4e4ca84f266f6d8480dfdd4ecea1a89
Michael O'Connor <oconnor97@aol.com>
2021-03-06 11:08:28 -0500

    js edits

--- Assets/script.js
+++ Assets/script.js
@@ -4,8 +4,7 @@ $('#searchBtn').on('click', function(event) {
     event.preventDefault();
     let inputValue = $('#query').val();
     let ticketmasterUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city='+inputValue+'&apikey=asJ5IIkFeppppkdFCPGgBB2cJYnYkfCT'
-    
-    
+
     fetch(ticketmasterUrl)
     .then(response => response.json())
     .then(data => {
@@ -17,7 +16,7 @@ $('#searchBtn').on('click', function(event) {
     
     
  // when event is clicked display the event name, venue and date of event
-    // let eventDetailsUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city=atlanta&apikey=asJ5IIkFeppppkdFCPGgBB2cJYnYkfCT'
+    
     fetch(ticketmasterUrl)
     .then(response => response.json())
     .then(data => {
@@ -37,7 +36,7 @@ $('#searchBtn').on('click', function(event) {
         })
          .then(response => response.json())
          .then( data =>
-            console.log(data))
+            console.log(data));
     });
 
 
@@ -45,14 +44,6 @@ $('#searchBtn').on('click', function(event) {
 
 });
 
-// testing the new docuManu API
-// let resturantUrl = 'https://api.documenu.com/v2/restaurant/4072702673999819?key=b0c70c0a6e86ee22a0cd19bb00e652d6'
-
-// fetch(resturantUrl)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//     })
 
 // view food button clicked display resturants nearby
 
